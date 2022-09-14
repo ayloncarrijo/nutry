65f365619320376318f69ffec7d862db8318e455
Aylon Carrijo <aylon.gabriel@outlook.com>
2022-09-14 14:44:09 -0300

    Auth

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,6 +1,8 @@
 import Button from "components/Button";
 import TextInput from "components/TextInput";
 import AuthLayout from "layouts/AuthLayout";
+import { useRouter } from "next/router";
+import { setCookie } from "nookies";
 import React from "react";
 import "twin.macro";
 import type { AppPage } from "types";
@@ -8,26 +10,21 @@ import type { AppPage } from "types";
 const Page: AppPage = () => {
   const [name, setName] = React.useState("");
 
-  // const { push } = useRouter();
+  const { push } = useRouter();
 
-  // const signIn = (event: React.FormEvent) => {
-  //   event.preventDefault();
+  const signIn = (event: React.FormEvent) => {
+    event.preventDefault();
 
-  //   setCookie(null, "userName", name, {
-  //     maxAge: Number.MAX_SAFE_INTEGER,
-  //   });
+    setCookie(null, "userName", name, {
+      maxAge: Number.MAX_SAFE_INTEGER,
+    });
 
-  //   push("/");
-  // };
+    void push("/");
+  };
 
   return (
     <div tw="min-h-screen flex justify-center items-center">
-      <form
-        tw="w-full max-w-xs"
-        onSubmit={() => {
-          //
-        }}
-      >
+      <form tw="w-full max-w-xs" onSubmit={signIn}>
         <TextInput
           label="Insira seu nome"
           value={name}
