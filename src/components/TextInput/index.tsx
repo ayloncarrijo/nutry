8f99f49ea62e66ab7af53c1598185195ff7f13c1
Aylon Carrijo <aylon.gabriel@outlook.com>
2022-09-14 14:41:54 -0300

    TextInput

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,32 +1,33 @@
+import clsx from "clsx";
 import React from "react";
 import "twin.macro";
 import tw from "twin.macro";
 import ReactUtil from "utils/ReactUtil";
 
 interface TextInputProps extends React.ComponentPropsWithoutRef<"input"> {
-  label?: string;
+  label: string;
 }
 
 const TextInput = ReactUtil.forwardRefWithAs<HTMLInputElement, TextInputProps>(
-  ({ onFocus, onBlur, label, ...props }, ref) => {
+  ({ onFocus, onBlur, label: rawLabel, ...props }, ref) => {
     const [isFocused, setIsFocused] = React.useState(false);
 
+    const label = clsx(rawLabel, props.required && "*");
+
     return (
-      <div>
-        {label && (
-          <label
-            tw="inline-flex mb-1 text-sm"
-            css={[isFocused && tw`text-blue-300`]}
-            htmlFor={props.id}
-          >
-            {label}
-            {props.required && " *"}
-          </label>
-        )}
+      <div tw="relative">
+        <fieldset
+          tw="absolute inset-0 top-px -mt-3 p-3 pointer-events-none rounded-md border border-gray-500"
+          css={[isFocused && tw`border-blue-300 text-blue-300`]}
+        >
+          <legend tw="-ml-1">
+            <span tw="px-1 text-sm">{label}</span>
+          </legend>
+        </fieldset>
 
         <input
           ref={ref}
-          tw="w-full ring-1 ring-gray-500 ring-inset bg-transparent rounded-lg py-3 px-4 focus:(outline-none ring-2 ring-blue-300)"
+          tw="w-full h-14 p-3 text-base text-white bg-transparent focus:(outline-none)"
           onFocus={(event) => {
             setIsFocused(true);
             onFocus?.(event);
