8f9e094607e958c95c3988f04ee260239436dadd
Aylon Carrijo <aylon.gabriel@outlook.com>
2022-09-21 17:19:54 -0300

    Clear

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import Container from "components/Container";
 import DietViewer from "components/DietViewer";
-import Selectable from "components/Selectable";
-import SnackCard from "components/SnackCard";
 import UserLayout from "layouts/UserLayout";
 import Api from "lib/api";
 import authenticate from "middlewares/authenticate";
@@ -21,59 +19,9 @@ interface PageProps {
 const Page: AppPage<PageProps> = ({ initialState }) => {
   const [dailyDiet, setDailyDiet] = React.useState(initialState.dailyDiet);
 
-  const [isChecked, setIsChecked] = React.useState(false);
-
   return (
     <Container>
       <DietViewer diet={dailyDiet} onDietChange={setDailyDiet} />
-
-      <div tw="grid grid-cols-1 gap-4 my-8">
-        <Selectable
-          checked={isChecked}
-          onChange={(e) => setIsChecked(e.target.checked)}
-        >
-          <SnackCard
-            carbohydrates={10}
-            fats={10}
-            measurement="G"
-            name="Amendoim"
-            proportion={100}
-            proteins={10}
-          />
-        </Selectable>
-
-        <Selectable
-          checked={false}
-          onChange={() => {
-            //
-          }}
-        >
-          <SnackCard
-            carbohydrates={10}
-            fats={10}
-            measurement="G"
-            name="Amendoim"
-            proportion={100}
-            proteins={10}
-          />
-        </Selectable>
-
-        <Selectable
-          checked={false}
-          onChange={() => {
-            //
-          }}
-        >
-          <SnackCard
-            carbohydrates={10}
-            fats={10}
-            measurement="G"
-            name="Amendoim"
-            proportion={100}
-            proteins={10}
-          />
-        </Selectable>
-      </div>
     </Container>
   );
 };
