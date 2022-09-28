b3b043780b33f5dea3786a388cc50a6507d8e108
Aylon Carrijo <aylon.gabriel@outlook.com>
2022-09-28 02:18:21 -0300

    History

--- src/components/SnackManager/SnackManagerModal/index.tsx
+++ src/components/SnackManager/SnackManagerModal/index.tsx
@@ -8,34 +8,47 @@ import type { Food } from "types/api";
 
 enum ModalStep {
   SNACK_TYPE,
-  SNACKS,
+  FOODS,
+  RECIPES,
   QUANTITY,
-}
-
-enum SnackType {
-  FOOD,
-  RECIPE,
 }
 
 function SnackManagerModal(): JSX.Element {
   const { isFoodOnly, closeModal } = useSnackManager();
 
-  const [modalStep, setModalStep] = React.useState(
-    isFoodOnly ? ModalStep.SNACKS : ModalStep.SNACK_TYPE
-  );
+  const [history, setHistory] = React.useState<Array<ModalStep>>([
+    isFoodOnly ? ModalStep.FOODS : ModalStep.SNACK_TYPE,
+  ]);
 
-  const [snackType, _setSnackType] = React.useState(SnackType.FOOD);
+  const modalStep = history[history.length - 1];
 
-  const setSnackType = (snackTypeIn: SnackType) => {
-    _setSnackType(snackTypeIn);
-    setModalStep(ModalStep.SNACKS);
+  if (modalStep == null) {
+    throw new Error("ModalStep is undefined");
+  }
+
+  const hasPrevious = history.length > 1;
+
+  const backStep = () => {
+    if (hasPrevious) {
+      setHistory((prevState) => prevState.slice(0, -1));
+    }
   };
+
+  const pushStep = (step: ModalStep) => {
+    setHistory((prevState) => [...prevState, step]);
+  };
+
+  const backButton = hasPrevious && (
+    <Button startIcon="arrow_back" variant="outlined" onClick={backStep}>
+      Voltar
+    </Button>
+  );
 
   const [selectedFood, _setSelectedFood] = React.useState<Food | null>(null);
 
   const setSelectedFood = (food: Food) => {
     _setSelectedFood(food);
-    setModalStep(ModalStep.QUANTITY);
+    pushStep(ModalStep.QUANTITY);
   };
 
   return {
@@ -48,14 +61,14 @@ function SnackManagerModal(): JSX.Element {
             <Button
               isFullWidth
               startIcon="fastfood"
-              onClick={() => setSnackType(SnackType.FOOD)}
+              onClick={() => pushStep(ModalStep.FOODS)}
             >
               Comida
             </Button>
             <Button
               isFullWidth
               startIcon="list_alt"
-              onClick={() => setSnackType(SnackType.RECIPE)}
+              onClick={() => pushStep(ModalStep.RECIPES)}
             >
               Receita
             </Button>
@@ -63,38 +76,14 @@ function SnackManagerModal(): JSX.Element {
         </div>
       </Modal>
     ),
-    [ModalStep.SNACKS]: (
-      <Modal
-        tw="w-full"
-        title={
-          {
-            [SnackType.FOOD]: "Comidas",
-            [SnackType.RECIPE]: "Receitas",
-          }[snackType]
-        }
-        onDismiss={closeModal}
-      >
-        {
-          {
-            [SnackType.FOOD]: (
-              <FoodViewer
-                onFoodClick={setSelectedFood}
-                startButton={
-                  !isFoodOnly && (
-                    <Button
-                      startIcon="arrow_back"
-                      variant="outlined"
-                      onClick={() => setModalStep(ModalStep.SNACK_TYPE)}
-                    >
-                      Voltar
-                    </Button>
-                  )
-                }
-              />
-            ),
-            [SnackType.RECIPE]: <div />,
-          }[snackType]
-        }
+    [ModalStep.FOODS]: (
+      <Modal tw="w-full" title="Comidas" onDismiss={closeModal}>
+        <FoodViewer onFoodClick={setSelectedFood} startButton={backButton} />
+      </Modal>
+    ),
+    [ModalStep.RECIPES]: (
+      <Modal tw="w-full" title="Receitas" onDismiss={closeModal}>
+        <div>...</div>
       </Modal>
     ),
     [ModalStep.QUANTITY]: (
