958a33d701c4b7345fb53357e386db077e5f4d43
Aylon Carrijo <aylon.gabriel@outlook.com>
2022-10-05 20:13:55 -0300

    Api URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,10 @@ import axios from "axios";
 
 const Api = {
   MAIN: axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/api"
+        : "https://diet-virid.vercel.app/api",
   }),
 };
 
